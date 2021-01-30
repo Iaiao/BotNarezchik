011383867f2ed460061bf3cb9a96371ce2092a42
Iaiao <err266228@gmail.com>
2021-01-30 17:09:26 +0200

    GUI

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const OAuth2 = google.auth.OAuth2;
 const yesno = require("yesno")
 const Jimp = require("jimp")
 const { Readable } = require("stream")
+const gi = require('node-gtk')
+const Gtk = gi.require('Gtk', '3.0')
 
 const DEBUG = process.env.DEBUG == "TRUE"
 
-const SCOPES = ['https://www.googleapis.com/auth/youtube.upload'];
-const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-const TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+const SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/'
+const TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json'
 
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
     if (err) {
@@ -71,130 +73,187 @@ function storeToken(token) {
             throw err;
         }
     }
-    fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) throw err;
-        console.log('Токен сохранён в ' + TOKEN_PATH);
-    })
+    fs.writeFileSync(TOKEN_PATH, JSON.stringify(token))
+    process.exit(0)
 }
 
-
-
 const vk = new VK({
-	  token: process.env.TOKEN
+    token: process.env.TOKEN
 })
 
+let win, status
 async function run(client) {
-    let response = await vk.api.wall.getById({
-        posts: "-170704076_" + process.env.POST
+    gi.startLoop()
+    Gtk.init()
+
+    const builder = Gtk.Builder.newFromFile(__dirname + "/ui.glade")
+    win = builder.getObject("mainWindow")
+    let post_url_entry = builder.getObject("postUrlEntry")
+    let button = builder.getObject("startButton")
+    let box = builder.getObject("box")
+
+    win.setDefaultSize(1280, 720)
+    win.setTitle("Бот Обрубка")
+    win.on("destroy", () => {
+        process.exit(0)
     })
-    let text = response[0].text
-    let utext = text.split(/\nОсновной: *\n/)[1].split(/\nНочной: *\n/)[0]
     let streams = {}
-    for(let entry of utext.split(/\n\s*\n/)) {
+    let checkboxes = {}
+    post_url_entry.on("changed", () => {
+        Object.values(checkboxes).forEach(widget => box.remove(widget))
+        checkboxes = {}
+        box.showAll()
+        if(/^(https?:\/\/)?(www\.)?vk\.com\/[0-9_a-zA-Z]+\?w=wall-?\d+_\d+$/i.test(post_url_entry.getText())) {
+            let post = post_url_entry.getText().split("?w=wall")[1]
+            parse(post).then(result => {
+                streams = result
+                for(let [stream, narezki] of Object.entries(streams)) {
+                    let i = 0
+                    for(let narezka of narezki) {
+                        let widget = Gtk.CheckButton.newWithLabel(narezka.name, "d")
+                        checkboxes[stream + " " + i++] = widget
+                        box.packStart(widget, true, true, 2)
+                    }
+                }
+                button.setSensitive(true)
+                box.showAll()
+            }).catch(err => {
+                console.error(err)
+            })
+        } else {
+            button.setSensitive(false)
+            box.showAll()
+        }
+    })
+    button.getChild().setMarkup('<span size="xx-large">Начать обрезание</span>')
+    button.setSensitive(false)
+    async function onButtonClick() {
+        let narezki = []
+        for(let [narezka, widget] of Object.entries(checkboxes)) {
+            narezki.push({
+                enabled: widget.active,
+                ...streams[narezka.split(" ")[0]][parseInt(narezka.split(" ")[1])]
+            })
+        }
+        narezki.push({time: "12:00:00"})
+        post_url_entry.setText("")
+        win.setTitle("Бот Обрубка (генерация интро)")
+        win.showAll()
+        await require("./introgen")(process.env.YTA_TOKEN)
+        let service = google.youtube('v3')
+        for(let i = 0; i < narezki.length - 1; i++) {
+            if(!narezki[i].enabled) continue
+            win.setTitle(`Обрубка #${i + 1} > ${narezki[i].name} (00:00:00.00)`)
+            win.showAll()
+            let interval = setInterval(() => {
+                win.setTitle(`Обрубка #${i + 1} > ${narezki[i].name} (${status})`)
+                win.show()
+            }, 100)
+            await upload(service, client, narezki[i], narezki[i + 1].time)
+            clearInterval(interval)
+        }
+        win.setTitle("Бот Обрубка")
+        win.showAll()
+        let dialog = new Gtk.MessageDialog(win, Gtk.DialogFlags.MODAL, Gtk.MessageType.INFO, Gtk.ButtonsType.NONE)
+        dialog.addButton("Готово", 42)
+        dialog.setDefaultSize(300, 1)
+        dialog.setTitle("Все нарезки опубликованы")
+        dialog.run()
+        dialog.destroy()
+    }
+    button.on("released", onButtonClick)
+    button.on("key-press-event", event => {
+        if(event.keyval == 65293 /* ENTER */) {
+            onButtonClick()
+        }
+    })
+
+    win.showAll()
+    Gtk.main()
+}
+
+async function parse(post) {
+    let response = await vk.api.wall.getById({
+        posts: post
+    })
+    let text = response[0]?.text
+    let utext = text?.split(/\nОсновной: *\n/)?.[1]?.split(/\nНочной: *\n/)?.[0] ?? text?.split(/\nНочной: *\n/)?.[0]
+    let streams = {}
+    for(let entry of utext?.split(/\n\s*\n/) ?? []) {
         try {
             let narezka = extract_metadata(entry)
             if(!streams[narezka.id]) streams[narezka.id] = []
             streams[narezka.id].push(narezka)
-            console.log(narezka)
         } catch(e) {
-            console.log("Ой, а как какать?\n" + e)
+            console.log("Ой, а как какать? " + e)
         }
     }
-    for(let [stream, narezki] of Object.entries(streams)) {
-        narezki.push({ time: "12:00:00" })
-        if(fs.existsSync("stream_" + stream + ".mkv")) {
-            console.log("Стрим уже скачан, обрезаю")
-            upload_all(narezki, stream, client)
-        } else {
-            console.log("Стрим начнёт скачиваться через 5 секунд")
-            await sleep(5)
-            console.log("Загружаю стрим https://youtu.be/" + stream)
-            let youtubedl = cp.spawn("youtube-dl", [
-                stream,
-                "--merge-output-format", "mkv",
-                "-o", "stream_" + stream,
-            ])
-            youtubedl.stdout.pipe(process.stdout)
-            youtubedl.on("exit", (code, signal) => {
-                if(code == 0) {
-                    console.log("Стрим успешно скачан.")
-                    upload_all(narezki, stream, client)
-                } else {
-                    console.error("Стрим не скачан с кодом " + code + " " + signal)
-                }
-            })
-        }
-    }
+    return streams
 }
 
-let allSelected = false
+function upload(service, client, narezka, time_end) {
+    return new Promise(async (resolve, reject) => {
+        console.log("Нарезка", narezka.name, narezka.time + "-" + time_end)
+        let proc_screenshot = cp.spawn("ffmpeg", [
+            "-ss", narezka.time, // начало
+            "-i", "stream_" + narezka.id + ".mkv",
+            "-ss", "00:25", // 25 секунд после начала
+            "-frames:v", "1",
+            "-q:v", "1",
+            "-f", "mjpeg",
+            "-"
+        ])
+        proc_screenshot.stdin.on("error", err => {
+            console.log("Ffmpeg завершил работу: " + err.name)
+        })
+        let thumbnail = create_thumbnail(toArray(proc_screenshot.stdout))
+        let proc_narezka = cp.spawn("ffmpeg", [
+            "-v", "quiet",
+            "-stats",
+            "-ss", narezka.time,
+            "-to", time_end,
+            "-i", "stream_" + narezka.id + ".mkv",
+            "-i", "intro.mov",
+            "-filter_complex", "[0:v][1:v]overlay=eof_action=pass[out]",
+            "-map:v", "[out]",
+            "-map", "0:a",
+            "-c:v", "libx264",
+            "-preset", "superfast",
+            "-f", "matroska",
+            "-"
+        ])
+        proc_narezka.stdin.on("error", err => {
+            console.log("Ffmpeg завершил работу: " + err.name)
+        })
+        proc_narezka.stderr.pipe(process.stderr)
+        proc_narezka.stderr.on("data", data => {
+            status = new String(data)?.split(" time=")?.[1]?.split(" ")?.[0]
+        })
 
-async function upload_all(narezki, stream, client) {
-    process.stderr.setMaxListeners(50)
-    let service = google.youtube('v3')
-    for(let i = 0; i < narezki.length - 1; i++) {
-        let narezka = narezki[i];
-        console.log("Нарезка", narezka.name, narezka.time + "-" + narezki[i + 1].time)
-        if(!(await yesno({
-            question: "Обрезать?"
-        }))) continue
-        when_all_selected().then(async () => {
-            let proc_screenshot = cp.spawn("ffmpeg", [
-                "-ss", narezka.time, // начало
-                "-i", "stream_" + stream + ".mkv",
-                "-ss", "00:25", // 25 секунд после начала
-                "-frames:v", "1",
-                "-q:v", "1",
-                "-f", "mjpeg",
-                "-"
-            ])
-            proc_screenshot.stdin.on("error", err => {
-                console.log("Ffmpeg завершил работу: " + err.name)
-            })
-            let thumbnail = create_thumbnail(toArray(proc_screenshot.stdout))
-            let proc_narezka = cp.spawn("ffmpeg", [
-                "-v", "quiet",
-                "-stats",
-                "-ss", narezka.time,
-                "-to", narezki[i + 1].time,
-                "-i", "stream_" + stream + ".mkv",
-                "-i", "intro.mov",
-                "-filter_complex", "[0:v][1:v]overlay[out]",
-                "-map:v", "[out]",
-                "-map:a", "0",
-                "-c:v", "libx264",
-                "-preset", "veryfast",
-                "-f", "matroska",
-                "-"
-            ])
-            proc_narezka.stdin.on("error", err => {
-                console.log("Ffmpeg завершил работу: " + err.name)
-            })
-            proc_narezka.stderr.pipe(process.stderr)
-        
-            if(DEBUG) {
-                console.log("Tags: ", generate_tags(narezka.name));
-                (await thumbnail).pipe(fs.createWriteStream("thumbnail.jpg"))
-                proc_narezka.stdout.pipe(fs.createWriteStream("narezka.mkv"))
-            } else {
-                console.log("Загружаю это на ютуб");
-                service.videos.insert({
-                    auth: client,
-                    autoLevels: true,
-                    notifySubscribers: false,
-                    stabilize: true,
-                    requestBody: {
-                        status: {
-                            embeddable: true,
-                            madeForKids: false,
-                            privacyStatus: "private",
-                            publishAt: new Date(Date.now() + i * 1000 * 60 * 60)
-                        },
-                        snippet: {
-                            title: narezka.name,
-                            description: `В этой нарезке - ${narezka.name}
+        if(DEBUG) {
+            console.log("Tags: ", generate_tags(narezka.name));
+            (await thumbnail).pipe(fs.createWriteStream("thumbnail.jpg"))
+            proc_narezka.stdout.pipe(fs.createWriteStream("narezka.mkv"))
+            console.log("Piping")
+            proc_narezka.on("close", resolve)
+        } else {
+            console.log("Загружаю это на ютуб");
+            service.videos.insert({
+                auth: client,
+                autoLevels: true,
+                notifySubscribers: false,
+                stabilize: true,
+                requestBody: {
+                    status: {
+                        embeddable: true,
+                        madeForKids: false,
+                        privacyStatus: "public"
+                    },
+                    snippet: {
+                        title: narezka.name,
+                        description: `В этой нарезке - ${narezka.name}
 Поставь лайк и подпишись!
-Стрим: https://youtu.be/${stream}?t=${timeToSeconds(narezka.time)}s
+Стрим: https://youtu.be/${narezka.id}?t=${timeToSeconds(narezka.time)}s
 
 === КАК ПОПАСТЬ В ВИДЕО ===
 Пиши комментарии:
@@ -205,51 +264,35 @@ async function upload_all(narezki, stream, client) {
 Спам и флуд = бан
 ТОП 3 попадают в следующую нарезку
 `,
-                            defaultAudioLanguage: "ru",
-                            defaultLanguage: "ru",
-                            tags: generate_tags(narezka.name)
+                        defaultAudioLanguage: "ru",
+                        defaultLanguage: "ru",
+                        tags: generate_tags(narezka.name)
+                    }
+                },
+                part: ["status", "snippet"],
+                media: {
+                    body: proc_narezka.stdout
+                }
+            }, async (err, video) => {
+                if(err) {
+                    reject("Возникла ошибка при загрузке видео: " + err)
+                } else {
+                    console.log(`Опубликована нарезка "${video.data.snippet.title}" (https://youtu.be/${video.data.id})`)
+                    service.thumbnails.set({
+                        auth: client,
+                        videoId: video.data.id,
+                        media: {
+                            body: await thumbnail
                         }
-                    },
-                    part: ["status", "snippet"],
-                    media: {
-                        body: proc_narezka.stdout
-                    }
-                }, async (err, video) => {
-                    if(err) {
-                        console.log("Возникла ошибка при загрузке видео:")
-                        console.error(err)
-                    } else {
-                        console.log(`Опубликована нарезка "${video.data.snippet.title}" (https://youtu.be/${video.data.id})`)
-                        service.thumbnails.set({
-                            auth: client,
-                            videoId: video.data.id,
-                            media: {
-                                body: await thumbnail
-                            }
-                        }).then(_ => {
-                            console.log("Превью загружено")
-                        }).catch(err => {
-                            console.log("Возникла ошибка при загрузке превью:")
-                            console.error(err)
-                        })
-                    }
-                })
-            }
-        })
-    }
-    allSelected = true
-}
-
-function when_all_selected() {
-    return new Promise(async (resolve, reject) => {
-        function a() {
-            if(allSelected) {
-                resolve()
-            } else {
-                setTimeout(a, 100)
-            }
+                    }).then(_ => {
+                        console.log("Превью загружено")
+                        resolve()
+                    }).catch(err => {
+                        reject("Возникла ошибка при загрузке превью: " + err)
+                    })
+                }
+            })
         }
-        setTimeout(a, 100)
     })
 }
 
@@ -273,7 +316,7 @@ function generate_tags(name) {
 }
 
 function create_thumbnail(screenshot) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async (resolve, _reject) => {
         let files = fs.readdirSync("frames/")
         let frame = files[Math.floor(Math.random() * files.length)]
         Jimp.read(Buffer.concat(await screenshot)).then(image => {
@@ -287,11 +330,11 @@ function create_thumbnail(screenshot) {
                         [ 1 / 2,     1 / 2,   1 / 2]
                     ])
                     .resize(1280, 720)
-                image.getBuffer(Jimp.MIME_JPEG, (err, buf) => {
+                image.getBuffer(Jimp.MIME_JPEG, (_err, buf) => {
                     let stream = new Readable({
                         read() {
-                            this.push(buf);
-                            this.push(null);
+                            this.push(buf)
+                            this.push(null)
                         }
                     })
                     resolve(stream)
@@ -306,12 +349,6 @@ function extract_metadata(entry) {
     let time = entry.split("\n")[0].split("(").pop().split(/\)/)[0].trim()
     let id = entry.split("\n")[1].split("https://youtu.be/")[1].split("?")[0]
     return { name, time, id }
-}
-
-function sleep(s) {
-    return new Promise(resolve => {
-        setTimeout(resolve, s * 1000)
-    })
 }
 
 function timeToSeconds(str) {
